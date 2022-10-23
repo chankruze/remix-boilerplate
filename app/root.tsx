f2248375c48de31ab7094fdc48b43cc6f3aaa0df
chankruze <chankruze@gmail.com>
2022-10-23 08:19:46 +0530

    feat: use nprogress to show transition state
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { useEffect } from "react";
 import styles from "./styles/app.css";
 import {
   Links,
@@ -7,7 +8,10 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useTransition,
 } from "@remix-run/react";
+import NProgress from "nprogress";
+import NProgressStyles from "nprogress/nprogress.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -16,10 +20,26 @@ export const meta: MetaFunction = () => ({
 });
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: styles }];
+  return [
+    { rel: "stylesheet", href: NProgressStyles },
+    { rel: "stylesheet", href: styles },
+  ];
 };
 
+// disable the loading spinner of nprogress
+NProgress.configure({ showSpinner: false });
+
 export default function App() {
+  const transition = useTransition();
+
+  useEffect(() => {
+    // when the state is idle then we can to complete the progress bar
+    if (transition.state === "idle") NProgress.done();
+    // and when it's something else it means it's either submitting a form or
+    // waiting for the loaders of the next location so we start it
+    else NProgress.start();
+  }, [transition.state]);
+
   return (
     <html lang="en">
       <head>
